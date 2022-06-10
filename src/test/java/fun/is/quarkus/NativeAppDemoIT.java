51cf7cb2845c5f0e78617b5dc7e034779aef40ac
MAQIL Abdelmouttalib <abdel.mouttalib.maqil@ibm.com>
2022-06-10 14:14:24 +0100

    first commit

---
+++ src/test/java/fun/is/quarkus/NativeAppDemoIT.java
@@ -0,0 +1,9 @@
+package fun.is.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAppDemoIT extends AppDemoTest {
+
+    // Execute the same tests but in native mode.
+}
